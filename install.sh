6ea93132edf2966da242f594ea01f85626ba46ac
rinalim <46272802+rinalim@users.noreply.github.com>
2021-05-04 16:08:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,13 @@
-sudo apt-get update
-sudo apt-get install libjpeg8 -y
-sudo apt-get install imagemagick -y
-sudo apt-get install fbi -y
-sudo apt-get install mplayer -y
-sudo pip3 install wheel
-sudo pip3 install keyboard
+pip3 install luma.oled
+pip3 install luma.lcd
+pip3 install python-resize-image
 
-sudo cp /usr/bin/fbi /usr/bin/fbi-marquee
+rm -rf /opt/retropie/configs/all/AdvanceMarquee/
+mkdir /opt/retropie/configs/all/AdvanceMarquee/
+cp -f -r ./AdvanceMarquee /opt/retropie/configs/all/
 
-rm -rf /opt/retropie/configs/all/PieMarqueeSPI/
-mkdir /opt/retropie/configs/all/PieMarqueeSPI/
-cp -f -r ./PieMarqueeSPI /opt/retropie/configs/all/
-
-sudo sed -i '/PieMarqueeSPI.py/d' /opt/retropie/configs/all/autostart.sh
-sudo sed -i '1i\\sudo /usr/bin/python3 /opt/retropie/configs/all/PieMarqueeSPI/PieMarqueeSPI.py &' /opt/retropie/configs/all/autostart.sh
+sudo sed -i '/AdvanceMarquee.py/d' /opt/retropie/configs/all/autostart.sh
+sudo sed -i '1i\\sudo /usr/bin/python3 /opt/retropie/configs/all/AdvanceMarquee/AdvanceMarquee.py &' /opt/retropie/configs/all/autostart.sh
 
 echo
 echo "Setup Completed. Reboot after 3 Seconds."
