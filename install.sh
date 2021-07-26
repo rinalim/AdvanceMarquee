ea4f0ad4b337d106827302d45ffabc6d607ca65d
rinalim <46272802+rinalim@users.noreply.github.com>
2021-07-26 11:43:39 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir /opt/retropie/configs/all/AdvanceMarquee/
 cp -f -r ./AdvanceMarquee /opt/retropie/configs/all/
 
 sudo sed -i '/AdvanceMarquee.py/d' /opt/retropie/configs/all/autostart.sh
-sudo sed -i '1i\\sudo /usr/bin/python3 /opt/retropie/configs/all/AdvanceMarquee/AdvanceMarquee.py &' /opt/retropie/configs/all/autostart.sh
+sudo sed -i '1i\\/usr/bin/python3 /opt/retropie/configs/all/AdvanceMarquee/AdvanceMarquee.py &' /opt/retropie/configs/all/autostart.sh
 
 echo
 echo "Setup Completed. Reboot after 3 Seconds."
